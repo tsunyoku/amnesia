ba95af7a6db26dd431c8321f5440f39fa6db63be
tsunyoku <mbruhyo@gmail.com>
2022-08-11 23:17:44 +0100

    fix routes not being included in router

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,4 +1,9 @@
-from fastapi.responses import ORJSONResponse
+from __future__ import annotations
+
 from fastapi import APIRouter
+from fastapi.responses import ORJSONResponse
+
+from . import oauth
+from . import register
 
 router = APIRouter(tags=["osu!lazer API"], default_response_class=ORJSONResponse)

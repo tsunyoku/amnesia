2b9c33dd37be423106871e71f5c0f15b798fcc82
tsunyoku <mbruhyo@gmail.com>
2022-08-11 23:26:32 +0100

    fix circular import on route initialisation

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+# initialize each of the routes
+from __future__ import annotations
+
+from .routes import oauth
+from .routes import register
--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -3,7 +3,4 @@ from __future__ import annotations
 from fastapi import APIRouter
 from fastapi.responses import ORJSONResponse
 
-from . import oauth
-from . import register
-
 router = APIRouter(tags=["osu!lazer API"], default_response_class=ORJSONResponse)
